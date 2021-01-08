d32c647e2f2be5c541191d0aee0b542e2ee4672f
Italo Emanoel <italoemanoel@intalo.net>
2021-01-08 18:23:16 -0300

    Domain model

---
+++ backend/src/main/java/com/intalo/dsdeliver/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.intalo.dsdeliver.entities;
+
+public enum OrderStatus {
+
+    PENDING, DELIVERED;
+    
+}
---
+++ backend/src/main/java/com/intalo/dsdeliver/entities/Product.java
@@ -0,0 +1,96 @@
+package com.intalo.dsdeliver.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Double price;
+    private String description;
+    private String imageUri;
+
+    public Product(long id, String name, Double price, String descriptions, String imageUri) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = descriptions;
+        this.imageUri = imageUri;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescriptions() {
+        return description;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.description = descriptions;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+}
